98a6fecd2dd0edec8df36bfe83ecca9d065570d1
佩岭 <2534856130@qq.com>
2022-03-29 00:32:14 +0800

    头像

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-       头像
+       <span :title="user.username">
+           {{slug}}
+       </span>
     </div>
 </template>
 
@@ -9,13 +11,30 @@
 export default{
   data(){
       return {
-
+          user:{
+              username:'hunger'
+          },
+          slug:'H'
       }
   }
 }
 </script>
 
 <style scoped>
-
+span{
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    text-align: center;
+    line-height: 32px;
+    border-radius: 50%;
+    background: #f2b81c;
+    color: #fff;
+    text-shadow: 1px 0 1px #795c19;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 18px;
+    margin-top: 15px;
+}
 </style>
 
